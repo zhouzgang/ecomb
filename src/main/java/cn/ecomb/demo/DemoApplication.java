0a4c1057b47c18bea1b363c21205f0fc3751935c
brian.zhou <brian.zhou@huolala.cn>
2020-07-21 17:54:44 +0800

    init demo

---
+++ src/main/java/cn/ecomb/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package cn.ecomb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.printf("1123");
+    }
+}
