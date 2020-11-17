9f48828d9ba009995a65e7531b05c30ce87a739f
brian.zhou <brian.zhou@huolala.cn>
2020-11-17 18:46:04 +0800

    添加分布式事务理解笔记

--- ecomb-park/ecomb-park-pool/src/main/java/cn/ecomb/park/pool/controller/HelloController.java
+++ ecomb-park/ecomb-park-pool/src/main/java/cn/ecomb/park/pool/controller/HelloController.java
@@ -1,14 +1,9 @@
 package cn.ecomb.park.pool.controller;
 
-import cn.ecomb.common.datas.redis.component.RedisHandle;
-import cn.ecomb.park.pool.constant.PoolConstant;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Random;
 
 /**
  * @author brian.zhou
