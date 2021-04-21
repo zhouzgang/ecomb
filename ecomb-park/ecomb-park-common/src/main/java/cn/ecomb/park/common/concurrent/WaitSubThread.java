3799a4c1f94b9bd0c14561be4def633b1ee95238
brian <brian.zhou@huolala.cn>
2021-04-21 23:39:25 +0800

    主线程等待子线程完成的各种例子

--- ecomb-park/ecomb-park-common/src/main/java/cn/ecomb/park/common/concurrent/WaitSubThread.java
+++ ecomb-park/ecomb-park-common/src/main/java/cn/ecomb/park/common/concurrent/WaitSubThread.java
@@ -8,4 +8,29 @@ package cn.ecomb.park.common.concurrent;
  */
 public class WaitSubThread {
 
+//	public void
+
+	public void useJoin() throws InterruptedException {
+		long start = System.currentTimeMillis();
+		Task task = new Task();
+		Thread thread = new Thread(task);
+		thread.start();
+		thread.join();
+		long end = System.currentTimeMillis();
+		System.out.println("runtime: " + (end - start));
+	}
+
+	class Task implements Runnable {
+		@Override
+		public void run() {
+			System.out.println(Thread.currentThread().getName() + "start");
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName() + "end");
+		}
+	}
+
 }
